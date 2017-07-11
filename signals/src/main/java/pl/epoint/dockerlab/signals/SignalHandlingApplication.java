2cf6a8b00bb82fa87f71c0fb2decf34283864f03
Kamil Murawski <kmurawski@e-point.pl>
2017-07-11 18:34:43 +0200

    Aplikacja przechwytująca sygnały testująca zawartość podanego parametrem katalogu.

--- signals/src/main/java/pl/epoint/dockerlab/signals/SignalHandlingApplication.java
+++ signals/src/main/java/pl/epoint/dockerlab/signals/SignalHandlingApplication.java
@@ -2,6 +2,8 @@ package pl.epoint.dockerlab.signals;
 
 import sun.misc.Signal;
 
+import java.io.File;
+
 /**
  * Example of signal handling.
  *
@@ -10,17 +12,32 @@ import sun.misc.Signal;
 public class SignalHandlingApplication {
 
     public static void main(String[] args) {
-        Signal.handle(new Signal("INT"), sig -> {
-            System.out.println(
-                    "\nApplication was interrupted!");
-        });
-        for(int i=0; i<100; i++) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new IllegalStateException(e);
+
+        if (args.length == 0) {
+            System.out.print("Directory name missing!");
+
+        } else {
+            String directoryName = args[0];
+            System.out.println("Looking for changes in directory: " + directoryName);
+            File folder = new File(directoryName);
+
+            Signal.handle(new Signal("INT"), sig -> {
+                System.out.println(
+                        "\nApplication was interrupted!");
+            });
+            while (true) {
+                File[] listOfFiles = folder.listFiles();
+                System.out.print("> ");
+                for (int i = 0; i < listOfFiles.length; ++i) {
+                    System.out.print(listOfFiles[i].getName() + " ");
+                }
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException(e);
+                }
+                System.out.println();
             }
-            System.out.print('.');
         }
     }
 }
