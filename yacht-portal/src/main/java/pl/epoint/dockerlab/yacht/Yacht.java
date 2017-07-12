71ae0d36acefbd142f54088cd2db05d1f3d6104c
Kamil Murawski <kmurawski@e-point.pl>
2017-07-12 16:44:56 +0200

    Dodanie integracji z bazą danych.

--- yacht-portal/src/main/java/pl/epoint/dockerlab/yacht/Yacht.java
+++ yacht-portal/src/main/java/pl/epoint/dockerlab/yacht/Yacht.java
@@ -1,14 +1,37 @@
 package pl.epoint.dockerlab.yacht;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * @author Kamil Murawski
  */
-@Data
-public class Yacht {
+@Entity
+@Getter
+class Yacht {
 
+    @Id
+    @GeneratedValue(generator = "yachts_seq")
+    @SequenceGenerator(name = "yachts_seq", initialValue = 1000, allocationSize = 1)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Setter
     private String name;
-    private Integer length;
+
+    @NotNull
+    @Setter
+    private Integer feet;
+
+    @NotNull
+    @Setter
     private Integer maxCrew;
 }
--- yacht-portal/src/main/java/pl/epoint/dockerlab/yacht/YachtController.java
+++ yacht-portal/src/main/java/pl/epoint/dockerlab/yacht/YachtController.java
@@ -1,11 +1,14 @@
 package pl.epoint.dockerlab.yacht;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author Kamil Murawski
@@ -14,13 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/yacht")
 public class YachtController {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Yacht> getYachts() {
+    private final YachtRepository yachtRepository;
 
-        Yacht y = new Yacht();
-        y.setLength(10);
-        y.setName("Duch");
-        y.setMaxCrew(7);
-        return new ResponseEntity<Yacht>(y, HttpStatus.OK);
+    @Autowired
+    public YachtController(YachtRepository yachtRepository) {
+        this.yachtRepository = yachtRepository;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<Yacht>> getYachts() {
+        return new ResponseEntity<List<Yacht>>(yachtRepository.findAll(), HttpStatus.OK);
     }
 }
